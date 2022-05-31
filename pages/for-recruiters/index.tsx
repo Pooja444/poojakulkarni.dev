14c362c458b3013e84d3127835e45f9d404d74c2
“Pooja <pakcutie14@gmail.com>
2022-05-30 22:59:20 -0700

    Initial commit for the for-recruiters component

--- pages/for-recruiters/index.tsx
+++ pages/for-recruiters/index.tsx
@@ -1,12 +1,31 @@
 import type { NextPage } from 'next'
 import NavBar from '../navbar'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Box, Typography } from '@mui/material'
 
 const ForRecruiters: NextPage = () => {
   return (
-    <NavBar></NavBar>
+    <Box>
+      <NavBar></NavBar>
+      <Box sx={{
+        mt: "70px",
+        textAlign: "center"
+      }}>
+        <Box sx={{
+          width: "100vw",
+          backgroundColor: "#427F8F",
+          p: "15px"
+        }}>
+          <Typography fontSize="1.2rem" color="white">
+            <b>Currently looking for roles:</b>
+            <Box component="span" sx={{
+              ml: "10px"
+            }}>
+              Software Engineer II (Full-Stack/Backend/API Development/Distributed Systems)
+            </Box>
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
